01f21600abefb41b8be728a2d34c6d39dbc5984f
Cato Auestad <bleakgadfly@outlook.com>
2013-12-09 04:24:15 -0800

    Added JS Code, Arrow image
    The watchface now gets the last closing stock price and sends it to the watchface on the ready event. Added a simple arrow to indicate whether the stock price have increased or decreased but not implemented the logic for this yet.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,22 +1,27 @@
 Pebble.addEventListener("ready",
 	function(e) {			
-		console.log("JS Loaded");
-		// Create interval
-		/** var req = new XMLHttpRequest();
+		var req = new XMLHttpRequest();
   		req.open('GET', 'http://www.netfonds.no/quotes/paperhistory.php?paper=EVRY.OSE&csv_format=csv', true);
   		req.onload = function(e) {
 			if (req.readyState == 4 && req.status == 200) {
 				if(req.status == 200) {
-					var response = JSON.parse(req.responseText); // split CSV
-					var temperature = result.list[0].main.temp;
-					var icon = result.list[0].main.icon;
-					Pebble.sendAppMessage({ "icon":icon, "temperature":temperature + "\u00B0C"}); // Create mapping for "icon" => integer
+					var response = req.responseText.split(/\r\n|\r|\n/g);
+					var lastClosing = response[1].split(",")[6];
+					var previousDayClosing = response[2].split(",")[6];
+					
+					if(previousDayClosing > lastClosing) {
+						Pebble.sendAppMessage({ "increasedValue":1, "stock_quote": "" + lastClosing + " | " + (Math.round((lastClosing - previousDayClosing) * 100) / 100) });
+					} else if(previousDayClosing < lastClosing) {
+						Pebble.sendAppMessage({ "increasedValue":2, "stock_quote": "" + lastClosing + " | +" + (Math.round((lastClosing - previousDayClosing) * 100) / 100) });
+					} else {
+						Pebble.sendAppMessage({ "increasedValue":0, "stock_quote": "" + lastClosing + " | 0.0" });
+					}					
 				} else { 
 					console.log("Error"); 
 				}
 			}
   		}
 	
-  		req.send(null); **/
+  		req.send(null);
 	}									
 );
--- src/main.c
+++ src/main.c
@@ -1,28 +1,33 @@
 #include "pebble.h"
-#define LAST_PRICE_SUM_ID 0
-#define DIFFERENTIAL_PERCENT_ID 1
-#define DIFFERENTIAL_SUM_ID 2
 	
 static Window *window; 
 static BitmapLayer *logo_bitmap_layer;
+static BitmapLayer *arrow_bitmap_layer;
+static GBitmap *arrow_gbitmap_container;
 static GBitmap *logo_gbitmap_container;
 static TextLayer *stock_quote_text_layer;
 static TextLayer *time_text_layer;
 
-//static BitmapLayer *arrow_bitmap_layer;
-//static GBitmap *arrow_gbitmap_container;
+const int INCREASED_VALUE = 1, STOCK_QUOTE = 2;
 
+static void message_handler(DictionaryIterator *iterator, void *context) {
+	Tuple* has_increased_value = dict_find(iterator, INCREASED_VALUE);
+	Tuple* current_stock_quote = dict_find(iterator, STOCK_QUOTE);
+	
+	if(has_increased_value) {
 
-//void message_handler(DictionaryIterator *iterator, void *context) {
-//	Tuple* lastPrice_sum = dict_find(iterator, LAST_PRICE_SUM_ID);
-//	Tuple* differential_in_percent = dict_find(iterator, DIFFERENTIAL_PERCENT_ID);
-//	Tuple* differential_sum = dict_find(iterator, DIFFERENTIAL_SUM_ID);
-//}
+	}
+	if(current_stock_quote) {
+		text_layer_set_text(stock_quote_text_layer, current_stock_quote->value->cstring);
+	}
+}
 
+static void handle_minute_tick(struct tm* tick_time, TimeUnits units_changed) {
 
-//void minute_tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-//	static char time_text[] = "00:00";
-//}
+  static char time_text[] = "00:00"; // Needs to be static because it's used by the system later.
+  strftime(time_text, sizeof(time_text), "%T", tick_time);
+  text_layer_set_text(time_text_layer, time_text);
+}
 
 static void text_layer_set_defaults(TextLayer *text_layer) {
 	text_layer_set_text_color(text_layer, GColorBlack);
@@ -31,9 +36,26 @@ static void text_layer_set_defaults(TextLayer *text_layer) {
 	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
 }
 
-static Layer* evry_logo_get_layer(GRect *rootBounds) {
+static Layer* arrow_get_layer(const GRect *rootBounds) {
+	arrow_gbitmap_container = gbitmap_create_with_resource(RESOURCE_ID_UP_ARROW);
+	
+	GRect arrowBounds = {
+		.origin = { .x = 0, .y = 0},
+		.size = arrow_gbitmap_container->bounds.size
+	};
+	
+	arrow_bitmap_layer = bitmap_layer_create(arrowBounds);
+	bitmap_layer_set_bitmap(arrow_bitmap_layer, arrow_gbitmap_container);
+	Layer* arrow_layer = bitmap_layer_get_layer(arrow_bitmap_layer);
+	
+	return arrow_layer;
+}
+
+static Layer* evry_logo_get_layer(const GRect *rootBounds) {
 	logo_gbitmap_container = gbitmap_create_with_resource(RESOURCE_ID_PEBBLE_EVRY_IMG);
 	
+	bitmap_layer_set_bitmap(arrow_bitmap_layer, arrow_gbitmap_container);
+		
 	int middleOfScreen = rootBounds->size.h / 2;
 	int halfSizeOfLogo = logo_gbitmap_container->bounds.size.h / 2;
 	int logoBeginPosition = middleOfScreen - halfSizeOfLogo;
@@ -51,29 +73,28 @@ static Layer* evry_logo_get_layer(GRect *rootBounds) {
 	return logo_layer;
 }
 
-static Layer* stock_quote_get_layer(GRect *rootBounds) {
+static Layer* stock_quote_get_layer(const GRect *rootBounds) {
 	GRect stock_quote_bounds = { 
 		.origin = { .x = rootBounds->origin.x, .y = rootBounds->origin.y + 10 }, 
 		.size = { .h = 30, .w = rootBounds->size.w }
-	};
-			
+	};	
+	
 	stock_quote_text_layer = text_layer_create(stock_quote_bounds);
-	text_layer_set_defaults(stock_quote_text_layer);
-	text_layer_set_text(stock_quote_text_layer, "9,30 | +0,5");
+	text_layer_set_defaults(stock_quote_text_layer);	
 	Layer *stock_quote_layer = text_layer_get_layer(stock_quote_text_layer);
 	
 	return stock_quote_layer;
 }
 
-static Layer* time_layer_get_layer(GRect *rootBounds) {
+static Layer* time_layer_get_layer(const GRect *rootBounds) {
 	GRect time_rect = {
-		.origin = { .x = rootBounds->origin.x, .y = rootBounds->size.h - 30 },
-		.size = { .h = 30, .w = rootBounds->size.w }
+		.origin = { .x = rootBounds->origin.x, .y = rootBounds->size.h - 50 },
+		.size = { .h = 50, .w = rootBounds->size.w }
 	};
 	
 	time_text_layer = text_layer_create(time_rect);
 	text_layer_set_defaults(time_text_layer);
-	text_layer_set_text(time_text_layer, "22:30");
+	text_layer_set_font(time_text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
 	Layer *time_layer = text_layer_get_layer(time_text_layer);
 	
 	return time_layer;
@@ -86,14 +107,22 @@ static void app_init(void) {
 	Layer *window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_frame(window_layer);
 
-	Layer *logo_layer = evry_logo_get_layer(&bounds);
 	Layer *stock_quote_layer = stock_quote_get_layer(&bounds);
+	Layer *arrow_layer = arrow_get_layer(&bounds);
+	Layer *logo_layer = evry_logo_get_layer(&bounds);	
 	Layer *time_layer = time_layer_get_layer(&bounds);
-
+	
 	layer_add_child(window_layer, logo_layer);
 	layer_add_child(window_layer, time_layer);
 	layer_add_child(window_layer, stock_quote_layer);			
-	
+	layer_add_child(window_layer, arrow_layer);			
+		
+	time_t now = time(NULL);
+  	struct tm *current_time = localtime(&now);
+  	handle_minute_tick(current_time, MINUTE_UNIT);
+  	tick_timer_service_subscribe(MINUTE_UNIT, &handle_minute_tick);
+	app_message_register_inbox_received(message_handler);
+	app_message_open(64, 64);
 	//tick_timer_service_subscribe(MINUTE_UNIT, minute_tick_handler);
 	//app_message_register_inbox_received(message_handler);
 }
@@ -101,10 +130,10 @@ static void app_init(void) {
 static void app_deinit(void) {	
 	text_layer_destroy(stock_quote_text_layer);
 	text_layer_destroy(time_text_layer);
-	//gbitmap_destroy(differential_image);
+	gbitmap_destroy(arrow_gbitmap_container);
 	gbitmap_destroy(logo_gbitmap_container);
-	//bitmap_layer_destroy(differential_indicator_layer);
-	bitmap_layer_destroy(logo_bitmap_layer);
+	bitmap_layer_destroy(arrow_bitmap_layer);
+	bitmap_layer_destroy(logo_bitmap_layer);	
 	window_destroy(window);
 }
 
